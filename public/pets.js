9bf349ffe7bbec508b581977934699f291e5f979
Stephanie Guiang <stephanieguiang0415@gmail.com>
2026-02-02 21:13:01 +0800

    Added pets.js

---
+++ public/pets.js
@@ -0,0 +1,148 @@
+import Dog_1 from "../src/assets/pet_cards/dog_1.jpg";
+import Dog_2 from "../src/assets/pet_cards/dog_2.jpg";
+import Dog_3 from "../src/assets/pet_cards/dog_3.jpg";
+import Dog_4 from "../src/assets/pet_cards/dog_4.jpg";
+import Dog_5 from "../src/assets/pet_cards/dog_5.jpg";
+import Dog_6 from "../src/assets/pet_cards/dog_6.jpg";
+import Dog_7 from "../src/assets/pet_cards/dog_7.jpg";
+import Dog_8 from "../src/assets/pet_cards/dog_8.webp";
+
+import Cat_1 from "../src/assets/pet_cards/cat_1.jpg";
+import Cat_2 from "../src/assets/pet_cards/cat_2.jpg";
+import Cat_3 from "../src/assets/pet_cards/cat_3.jpg";
+import Cat_4 from "../src/assets/pet_cards/cat_4.jpg";
+import Cat_5 from "../src/assets/pet_cards/cat_5.jpg";
+
+const PET_DATA = [
+  {
+    id: 11,
+    name: "Marshmallow",
+    animal_type: "Dog",
+    breed: "Aspin",
+    age: "2 years",
+    image: Dog_1,
+    status: "Available"
+  },
+
+  {
+    id: 12,
+    name: "Luna",
+    breed: "Golden Retriever",
+    animal_type: "Dog",
+    age: "4 months",
+    image: Dog_2,
+    status: "Available"
+  },
+
+   {
+    id: 13,
+    name: "Sol",
+    breed: "Aspin",
+    animal_type: "Dog",
+    age: "6 months",
+    image: Dog_3,
+    status: "Available"
+  },
+
+  {
+    id: 14,
+    name: "Blacky",
+    breed: "Aspin",
+    animal_type: "Dog",
+    age: "6 months",
+    image: Dog_4,
+    status: "Available"
+  },
+
+  {
+    id: 15,
+    name: "Bantay",
+    breed: "Aspin",
+    animal_type: "Dog",
+    age: "6 months",
+    image: Dog_5,
+    status: "Available"
+  },
+
+  {
+    id: 16,
+    name: "Creamy",
+    breed: "Aspin",
+    animal_type: "Dog",
+    age: "10 months",
+    image: Dog_6,
+    status: "Available"
+  },
+
+  {
+    id: 17,
+    name: "Pulag",
+    breed: "Aspin",
+    animal_type: "Dog",
+    age: "6 months",
+    image: Dog_7,
+    status: "Available"
+  },
+
+  {
+    id: 18,
+    name: "Gaara",
+    breed: "Aspin",
+    animal_type: "Dog",
+    age: "6 months",
+    image: Dog_8,
+    status: "Available"
+  },
+
+  {
+    id: 21,
+    name: "Watermelon",
+    breed: "Puspin",
+    animal_type: "Cat",
+    age: "6 months",
+    image: Cat_1,
+    status: "Available"
+  },
+
+  {
+    id: 22,
+    name: "Sugar",
+    breed: "Puspin",
+    animal_type: "Cat",
+    age: "6 months",
+    image: Cat_2,
+    status: "Available"
+  },
+
+  {
+    id: 23,
+    name: "Banana",
+    breed: "Puspin",
+    animal_type: "Cat",
+    age: "6 months",
+    image: Cat_3,
+    status: "Available"
+  },
+
+  {
+    id: 24,
+    name: "Mango",
+    breed: "Puspin",
+    animal_type: "Cat",
+    age: "6 months",
+    image: Cat_4,
+    status: "Available"
+  },
+
+  {
+    id: 25,
+    name: "Apple",
+    breed: "Puspin",
+    animal_type: "Cat",
+    age: "6 months",
+    image: Cat_5,
+    status: "Available"
+  },
+];
+
+export default PET_DATA;
